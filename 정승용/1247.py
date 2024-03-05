00b23d0488276fcd220f9e286c80661e00df0c56
freamwork97 <jsy11123@naver.com>
2024-03-05 14:52:54 +0900

    chore:update 1247.py_Seungyong

--- 정승용/1247.py
+++ 정승용/1247.py
@@ -15,15 +15,24 @@ input = sys.stdin.readline  # 문자열 하나씩 입력받기(시간 초과 방
 result = []  # 최종 결과 저장 빈 리스트
 for _ in range(3):  # 3번 반복
     n = int(input())  # 반복마다 입력할 정수 값 초기화
-    s = [
-        int(input()) for _ in range(n)
-    ]  # n번 만큼 반복하여 정수 입력하여 s에 리스트로 저장
-    if sum(s) == 0:
+    s = 0
+    for _ in range(n):
+        s += int(input())
+    if s == 0:
         result.append(0)  # s의 합이 0이면 result에 0저장
-    elif sum(s) > 0:
+    elif s > 0:
         result.append("+")  # s가 0보다 크면 result에 +저장
     else:
         result.append("-")  # s가 0보다 작으면 result에 -저장
+    # s = [
+    #     int(input()) for _ in range(n)
+    # ]  # n번 만큼 반복하여 정수 입력하여 s에 리스트로 저장
+    # if sum(s) == 0:
+    #     result.append(0)  # s의 합이 0이면 result에 0저장
+    # elif sum(s) > 0:
+    #     result.append("+")  # s가 0보다 크면 result에 +저장
+    # else:
+    #     result.append("-")  # s가 0보다 작으면 result에 -저장
 
 # 최종 출력
 for i in result:
